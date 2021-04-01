97e7ce62f24c1d2937b722366df71fd1ac36b869
Alex Marshall <alex.k.marshall83@gmail.com>
2021-04-01 09:02:25 +0100

    Display gang roster

--- client/src/pages/gang.tsx
+++ client/src/pages/gang.tsx
@@ -1,14 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useReadGang } from "../hooks/gangs";
-
+import { GangRoster } from "../components/gang-roster";
+import { useMemo } from "react";
+import { Gang as GangType } from "../schemas";
 interface RouteParams {
   id: string;
+}
+
+function formatGang({
+  name,
+  faction: { name: factionName },
+  rating,
+  reputation,
+  wealth,
+  territories,
+  fighters,
+  stash: { credits },
+}: GangType) {
+  return {
+    name,
+    house: factionName,
+    rating,
+    reputation,
+    wealth,
+    territories,
+    fighters: [],
+    stash: [`${credits} credits`],
+  };
 }
 export function Gang() {
   const { id } = useParams<RouteParams>();
   const { isError, error, gang } = useReadGang(id);
 
+  const formattedGang = useMemo(() => formatGang(gang), [gang]);
+
   if (isError) return <pre>{JSON.stringify(error)}</pre>;
 
-  return <div>{JSON.stringify(gang)}</div>;
+  return (
+    <div>
+      <GangRoster {...formattedGang} />;
+    </div>
+  );
 }
